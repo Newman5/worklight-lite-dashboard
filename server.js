7fb9a12791467cfb47e0584bc76d6e708f2111b1
Newman <nslanier@yahoo.com>
2025-07-30 19:11:16 -0400

    to host the dashboard on github

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const fs = require('fs');
 const path = require('path');
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 // Serve dashboard.html at root
 app.get('/', (req, res) => {
